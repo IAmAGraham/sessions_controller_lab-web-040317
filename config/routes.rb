8192156a74bb670a9b5112b3fd1e9f96c3e27845
github username <github email address>
2017-05-03 16:55:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  # root 'application#hello'
+  get '/sessions/new'
+  root 'sessions#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
